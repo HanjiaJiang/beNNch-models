aed4a27e03a62ae5cd7bb2f68938978159122733
jiang4 <jiang4@jrlogin07.jureca>
2024-09-02 16:50:35 +0200

    Add model switch in bench_template.py

--- astro/bench_template.py
+++ astro/bench_template.py
@@ -7,7 +7,7 @@ astrocyte_lr_1994 model. This script is used for HPC benchmarks.
 
 """
 
-from network import model_default, build_network, run_simulation
+from network import model_default, run_simulation
 
 
 ###############################################################################
@@ -15,6 +15,7 @@ from network import model_default, build_network, run_simulation
 # Define all relevant parameters: changes should be made here
 
 params = {
+    'model': {model_name},             # model name
     'nvp': {num_vps},                  # total number of virtual processes
     'scale': {N_SCALING},              # scaling factor of the network size
     'simtime': {model_time_sim},       # total simulation time in ms
@@ -26,11 +27,59 @@ params = {
 }
 
 def run():
-    # define conn_spec parameters
-    model_update_dict = {
-        "conn_params_e": {"rule": "pairwise_bernoulli", "p": 0.1/params["scale"]},
-        "conn_params_i": {"rule": "pairwise_bernoulli", "p": 0.1/params["scale"]},
-    }
+    # define model
+    model = params["model"]
+
+    # define model_update_dict according to specified model
+    N_ex = model_default["network_params"]["N_ex"]
+    N_in = model_default["network_params"]["N_in"]
+    p = model_default["conn_params_e"]["p"]
+    if model == "Bernoulli":
+        model_update_dict = {
+            "conn_params_e": {"rule": "pairwise_bernoulli", "p": p/params["scale"]},
+            "conn_params_i": {"rule": "pairwise_bernoulli", "p": p/params["scale"]},
+        }
+    elif model == "Synchronous":
+        model_update_dict = {
+            "conn_params_e": {"rule": "pairwise_bernoulli", "p": p/params["scale"]},
+            "conn_params_i": {"rule": "pairwise_bernoulli", "p": p/params["scale"]},
+            "syn_params": {
+                "w_a2n": 0.01,  # weight of astrocyte-to-neuron connection
+                "w_e": 1.0,  # weight of excitatory connection in nS
+                "w_i": -4.0,  # weight of inhibitory connection in nS
+                "d_e": 2.0,  # delay of excitatory connection in ms
+                "d_i": 2.0,  # delay of inhibitory connection in ms
+            },
+            "neuron_params_ex": {
+                "tau_syn_ex": 2.0,  # excitatory synaptic time constant in ms
+                "tau_syn_in": 2.0,  # inhibitory synaptic time constant in ms
+            },
+            "neuron_params_in": {
+                "tau_syn_ex": 2.0,  # excitatory synaptic time constant in ms
+                "tau_syn_in": 2.0,  # inhibitory synaptic time constant in ms
+            },
+        }
+    elif model == "Fixed-indegree":
+        model_update_dict = {
+            "conn_params_e": {"rule": "fixed_indegree", "indegree": N_ex*p}
+            "conn_params_i": {"rule": "fixed_indegree", "indegree": N_in*p}
+        }
+    elif model == "Fixed-outdegree":
+        model_update_dict = {
+            "conn_params_e": {"rule": "fixed_outdegree", "outdegree": (N_ex+N_in)*p}
+            "conn_params_i": {"rule": "fixed_outdegree", "outdegree": (N_ex+N_in)*p}
+        }
+    elif model == "Fixed-total-number":
+        model_update_dict = {
+            "conn_params_e": {"rule": "fixed_total_number", "N": int(N_ex*(N_ex+N_in)*p)}
+            "conn_params_i": {"rule": "fixed_total_number", "N": int(N_ex*(N_ex+N_in)*p)}
+        }
+    else:
+        print("No correct model specified; use default (Beroulli).")
+        model_update_dict = {
+            "conn_params_e": {"rule": "pairwise_bernoulli", "p": p/params["scale"]},
+            "conn_params_i": {"rule": "pairwise_bernoulli", "p": p/params["scale"]},
+        }
 
     # run simulation
     run_simulation(params, model_update_dict)
